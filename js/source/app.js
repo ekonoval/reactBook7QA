366c80d723bc3861a878e00bcca8dde3f12f1520
Yehor Konoval <ekonoval@gmail.com>
2017-10-29 17:48:55 +0200

    integrate JSX Excel component from final chapter 4 file

--- js/source/app.js
+++ js/source/app.js
@@ -3,10 +3,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Logo from './components/Logo';
+import Excel from './components/Excel';
+
+var headers = localStorage.getItem('headers');
+var data = localStorage.getItem('data');
+
+if (!headers) {
+  headers = ['Title', 'Year', 'Rating', 'Comments'];
+  data = [['Test', '2015', '3', 'meh']];
+}
+
+// var headers = ['Title', 'Year', 'Rating', 'Comments'];
+// var initialData = [['Test', '2015', '3', 'meh']];
 
 ReactDOM.render(
-  <h1>
-    <Logo /> Welcome to The App!
-  </h1>,
+  <div>
+    <h1>
+      <Logo /> Welcome to The App!
+      <Excel headers={headers} initialData={data} />
+    </h1>
+  </div>,
   document.getElementById('pad')
 );
