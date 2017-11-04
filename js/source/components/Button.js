a324eb547ecaf9599a969d5014521cafbc82f79c
Yehor Konoval <ekonoval@gmail.com>
2017-11-04 20:09:41 +0200

    Button old style alternative

--- js/source/components/Button.js
+++ js/source/components/Button.js
@@ -12,4 +12,34 @@ function Button(props) {
 Button.propTypes = {
   href: React.PropTypes.string,
 };
+
+/* old style alternative
+
+var Button = React.createClass({
+  propTypes: {
+    href: React.PropTypes.string
+  },
+
+  render: function() {
+    const cssclasses = classNames('Button', this.props.className);
+    return this.props.href
+      ? <a {...this.props} className={cssclasses}/>
+      : <button {...this.props} className={cssclasses}/>;
+  }
+});
+*/
+
+/* new short way
+
+const Button = props =>
+  props.href
+    ? <a {...props} className={classNames('Button', props.className)}/>
+    : <button {...props} className={classNames('Button', props.className)}/>
+
+Button.propTypes = {
+  href: PropTypes.string,
+};
+
+*/
+
 export default Button;
