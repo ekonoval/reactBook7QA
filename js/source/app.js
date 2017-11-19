f06e2cdac3c01cf3ee50bf5592725b5f9c1c7d02
Yehor Konoval <ekonoval@gmail.com>
2017-11-19 18:07:57 +0200

    excel + app.js

--- js/source/app.js
+++ js/source/app.js
@@ -5,22 +5,33 @@ import ReactDOM from 'react-dom';
 import Logo from './components/Logo';
 import ExcelOld from './components/ExcelOld';
 
-var headers = localStorage.getItem('headers');
-var data = localStorage.getItem('data');
+import Excel from './complexComponents/Excel';
+import schema from './schema';
 
-if (!headers) {
-  headers = ['Title', 'Year', 'Rating', 'Comments'];
-  data = [['Test', '2015', '3', 'meh']];
-}
+// var headers = localStorage.getItem('headers');
+// var data = localStorage.getItem('data');
+//
+// if (!headers) {
+//   headers = ['Title', 'Year', 'Rating', 'Comments'];
+//   data = [['Test', '2015', '3', 'meh']];
+// }
 
-// var headers = ['Title', 'Year', 'Rating', 'Comments'];
-// var initialData = [['Test', '2015', '3', 'meh']];
+let data = {};
+schema.forEach((item) => data[item.id] = item.sample);
+data = [data];
 
 ReactDOM.render(
   <div>
     <h1>
       <Logo /> Welcome to The App!
-      <ExcelOld headers={headers} initialData={data} />
+      {/*<ExcelOld headers={headers} initialData={data} />*/}
+
+      <h2>Excel new</h2>
+      <Excel
+        schema={schema}
+        initialData={data}
+      />
+
     </h1>
   </div>,
   document.getElementById('pad')
