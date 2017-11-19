aba52a61717b08b98bfab8a194d560fc96b5f5a1
Yehor Konoval <ekonoval@gmail.com>
2017-11-19 13:21:10 +0200

    Dialog

---
+++ js/source/components/Dialog.js
@@ -0,0 +1,56 @@
+import React, {Component, PropTypes} from 'react';
+import Button from './Button';
+
+class Dialog extends Component {
+  render() {
+    return (
+      <div className={this.props.modal ? 'Dialog DialogModal' : 'Dialog'}>
+        <div className={this.props.modal ? 'DialogModalWrap' : null}>
+          <div className="DialogHeader">{this.props.header}</div>
+          <div className="DialogBody">{this.props.children}</div>
+          <div className="DialogFooter">
+            {this.props.hasCancel
+              ? <span
+                  className="DialogDismiss"
+                  onClick={this.props.onAction.bind(this, 'dismiss')}>
+                  Cancel
+                </span>
+              : null
+            }
+            <Button onClick={this.props.onAction.bind(this,
+                this.props.hasCancel ? 'confirm' : 'dismiss')}>
+              {this.props.confirmLabel}
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    if (this.props.modal) {
+      document.body.classList.add('DialogModalOpen');
+    }
+  }
+
+  componentWillUnmount() {
+    document.body.classList.remove('DialogModalOpen');
+  }
+}
+
+Dialog.propTypes = {
+  header: PropTypes.string.isRequired,
+  confirmLabel: PropTypes.string,
+  modal: PropTypes.bool,
+  onAction: PropTypes.func,
+  hasCancel: PropTypes.bool,
+};
+
+Dialog.defaultProps = {
+  confirmLabel: 'ok',
+  modal: false,
+  onAction: () => {},
+  hasCancel: true
+};
+
+export default Dialog;
--- js/source/discover.js
+++ js/source/discover.js
@@ -9,11 +9,28 @@ import Suggest from './components/Suggest';
 import Rating from './components/Rating';
 import FormInput from './components/FormInput';
 import Form from './components/Form';
-import Actions from './components/Actions'
+import Actions from './components/Actions';
+import Dialog from './components/Dialog';
 
 ReactDOM.render(
   <div style={{padding: '20px'}}>
     <h1>Component discoverer</h1>
+
+    <h2>Dialogs</h2>
+    <Dialog
+      header="Out-of-the-box example"
+      onAction={type => alert(type)}>
+      Hello, dialog!
+    </Dialog>
+
+    <Dialog
+      header="No cancel, custom button"
+      hasCancel={false}
+      confirmLabel="Whatever"
+      onAction={type => alert(type)}>
+      Anything goes here, see:
+      <Button>A button</Button>
+    </Dialog>
 
     <h2>Actions</h2>
     <div><Actions onAction={type => alert(type)} /></div>
