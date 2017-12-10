dcc37755c01c68f7638dc576aa20d7bbb9bf1644
Yehor Konoval <ekonoval@gmail.com>
2017-12-10 14:51:03 +0200

    build debug

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,20 @@
+#!/usr/bin/env bash
 # js transform
+echo "^^ statted at "; date;
+
 babel --presets react,es2015 js/source/ -d js/build
+echo "---->>> babel done"; date;
 
 # js package
 browserify js/build/app.js -o bundle.js
+echo "---->>> app.js done"; date;
+
 browserify js/build/discover.js -o discover-bundle.js
+
+echo "---->>> discover.js done"; date;
 
 # css package
 cat css/*/* css/*.css | sed 's/..\/..\/images/images/g' > bundle.css
 # done
-date; echo;
+echo "------------------ done ------------------";
 
