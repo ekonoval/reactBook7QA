72b7acd5ac6a5abab10582c08b21fcffdf5c94d5
Yehor Konoval <ekonoval@gmail.com>
2017-12-25 17:10:00 +0200

    update FormInputField by copying from the ReactApp stoyan

--- js/source/components/FormInput.js
+++ js/source/components/FormInput.js
@@ -1,51 +1,63 @@
-import React, {Component, PropTypes} from 'react';
+/* @flow */
+
 import Rating from './Rating';
+import React, {Component} from 'react';
 import Suggest from './Suggest';
 
+type FormInputFieldType = 'year' | 'suggest' | 'rating' | 'text' | 'input';
+
+export type FormInputFieldValue = string | number;
+
+export type FormInputField = {
+  type: FormInputFieldType,
+  defaultValue?: FormInputFieldValue,
+  id?: string,
+  options?: Array<string>,
+  label?: string,
+};
+
 class FormInput extends Component {
+
+  props: FormInputField;
+
+  static defaultProps = {
+    type: 'input',
+  };
+
+  getValue(): FormInputFieldValue {
+    return 'value' in this.refs.input
+      ? this.refs.input.value
+      : this.refs.input.getValue();
+  }
+
   render() {
-    const common = {
+    const common: Object = {
       id: this.props.id,
       ref: 'input',
-      defaultValue: this.props.defaultValue
+      defaultValue: this.props.defaultValue,
     };
-
     switch (this.props.type) {
       case 'year':
         return (
           <input
             {...common}
             type="number"
-            defaultValue={this.props.defaultValue || new Date().getFullYear()}
-          />
+            defaultValue={this.props.defaultValue || new Date().getFullYear()} />
         );
-
       case 'suggest':
-        return <Suggest {...common} options={this.props.options}/>;
-
+        return <Suggest {...common} options={this.props.options} />;
       case 'rating':
-        return <Rating {...common} defaultValue={this.props.defaultValue}/>;
+        return (
+          <Rating
+            {...common}
+            defaultValue={parseInt(this.props.defaultValue, 10)} />
+        );
       case 'text':
-        return <textarea {...common}/>;
+        return <textarea {...common} />;
       default:
-        return <input {...common} type="text"/>;
+        return <input {...common} type="text" />;
     }
-
-    return null;
-  }
-
-  getValue() {
-    return 'value' in this.refs.input
-      ? this.refs.input.value
-      : this.refs.input.getValue();
   }
 }
 
-FormInput.propTypes = {
-  type: PropTypes.oneOf(['year', 'suggest', 'rating', 'text', 'input']),
-  id: PropTypes.string,
-  options: PropTypes.array,
-  defaultValue: PropTypes.any
-};
-
-export default FormInput;
+export default FormInput
